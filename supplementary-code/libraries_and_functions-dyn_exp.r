fea61ace74ad98c9d7eeb78caeb14ac7913c5c3a
Alexandra Paxton <paxton.alexandra@gmail.com>
2017-06-08 16:52:06 -0700

    Add missing requirements

--- supplementary-code/libraries_and_functions-dyn_exp.r
+++ supplementary-code/libraries_and_functions-dyn_exp.r
@@ -4,7 +4,7 @@
 # additional functions to facilitate data prep and analysis.
 #
 # Written by: A. Paxton (University of California, Berkeley)
-# Date last modified: 26 July 2016
+# Date last modified: 8 June 2017
 #
 #####################################################################################
 
@@ -19,6 +19,8 @@ library(ggplot2)
 library(beepr)
 library(reshape2)
 library(LSAfun)
+library(gtable)
+library(gridExtra)
 
 #####################################################################################
 
@@ -114,3 +116,56 @@ import_transcript_files = function(transcript_file){
     
 # "lookfor": Identify variables within a dataframe that appear within a specified list
 lookfor = function(d,p) names(d)[grep(p,names(d))]
+    
+    
+# 'pander_lme': Create a cleaner lme4 model output with pander.
+#
+# Optional (requires 'MuMIn'): 
+#   Include caption with marginal (i.e., fixed effects only) 
+#   and conditional (i.e., fixed and random effects) R-squared.
+# Available from https://github.com/a-paxton/stats-tools/
+    
+pander_lme = function(lme_model_name, stats.caption){
+  
+  # load in pander
+  require(pander)
+  
+  # disable scientific notation
+  options(scipen = 999)
+  
+  # convert the model summary to a dataframe
+  neat_output = data.frame(summary(lme_model_name)$coefficient)
+  
+  # round p-values (using Psychological Science's recommendations) for display
+  neat_output$p = 2*(1-pnorm(abs(neat_output$t.value)))
+  neat_output$p[neat_output$p < .0001] = .0001
+  neat_output$p[neat_output$p >= .0001] = round(neat_output$p[neat_output$p >= .0001],4)
+  neat_output$p[neat_output$p >= .0005] = round(neat_output$p[neat_output$p >= .0005],3)
+  neat_output$p[neat_output$p >= .25] = round(neat_output$p[neat_output$p >= .25],2)
+  
+  # create significance and trending markers
+  neat_output$sig = ' '
+  neat_output$sig[neat_output$p < .1] = '.'
+  neat_output$sig[neat_output$p < .05] = '*'
+  neat_output$sig[neat_output$p < .01] = '**'
+  neat_output$sig[neat_output$p < .001] = '***'
+  
+  # if desired, create a caption that includes R-squared
+  if (stats.caption == TRUE){
+    
+    # use MuMIN to calculate R-squared
+    require(MuMIn)
+    model_marginal_r_squared = r.squaredGLMM(lme_model_name)[['R2m']]
+    model_conditional_r_squared = r.squaredGLMM(lme_model_name)[['R2c']]
+    neat_caption = paste('**Marginal *R*-squared: ',
+                         round(model_marginal_r_squared,3), 
+                         ". Conditional *R*-squared: ",
+                         round(model_conditional_r_squared,3),".**",sep="")
+    
+    # return the table
+    return(pander(neat_output, split.table = Inf, caption = neat_caption, style = 'rmarkdown'))
+    
+  } else { # or return a table without it
+    return(pander(neat_output, split.table = Inf, style = 'rmarkdown'))
+  }
+}
